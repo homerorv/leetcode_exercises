80ee7b3e726656c9ffb426256aaba9bd405baf0a
homerorv <rebeca.rios21@gmail.com>
2026-02-09 08:57:47 -0600

    more exercises

---
+++ top_interview_150/python/linked_list/92._reverse_linked_list_II.py
@@ -0,0 +1,64 @@
+'''
+92. Reverse Linked List II
+https://leetcode.com/problems/reverse-linked-list-ii/?envType=study-plan-v2&envId=top-interview-150
+
+Given the head of a singly linked list and two integers left and right where left <= right,
+reverse the nodes of the list from position left to position right, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+               1,3,2,4,5
+               1,3,4,2,5
+               1,4,3,2
+Output: [1,4,3,2,5]
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        index = 1
+        node_left = head
+        prev_node_left = head
+        
+        while index < left:
+            prev_node_left = node_left
+            node_left = node_left.next
+            index += 1
+        node_right = node_left
+
+        while index < right:
+            node_right = node_right.next
+            index += 1
+
+        node_right = node_right.next
+
+
+
+        while node_left != node_right:
+            current = node_left
+            temp = node_right.next
+            prev_node_left.next = current.next
+            node_left = prev_node_left.next
+            node_right.next =current
+            current.next = temp
+            node_right = current
+        return head
+    
+    
+
+l1 = ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5,ListNode(6))))))
+
+sol = Solution()
+head = sol.reverseBetween(l1,2,4)
+
+while(head != None):
+    print(head.val)
+    head = head.next
+
+        
+
+
---
+++ top_interview_150/python/matrix/36_valid_sudoku.py
@@ -0,0 +1,60 @@
+'''
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules. 
+'''
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        squares = [set() for _ in range(9)]
+
+        for x in range(9):
+            for y in range(9):
+                val = board[x][y]
+                if not val.isnumeric():
+                    continue
+
+                if val in rows[x]:
+                    return False
+                rows[x].add(val)
+                    
+                if val in cols[y]:
+                    return False
+                cols[y].add(val)
+
+                pos_square = ((x//3) * 3) + y//3
+
+                if val in squares[pos_square]:
+                    return False
+                squares[pos_square].add(val)
+
+        return True
+
+'''
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+'''
+
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+sol = Solution()
+result = sol.isValidSudoku(board)
+
+print("Result: ",result)
+
+
